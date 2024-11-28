028dc974fe5c265eae0eba95b3ef883cbbb5af14
Samuel <themainsamuel@gmail.com>
2024-11-28 16:58:31 +0100

    first  attempt

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# function to find the minimum number of coins required to make up a given total amount, given a list of coin denominations
+
+def min_coins(coins, total):
+    # create a list to store the minimum number of coins required for each amount from 0 to
+    # the given total amount
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0  # 0 coins are required to make up an amount
+    if total <= 0:
+        return 0
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+            return dp[total] if dp[total] != float('inf') else -1
+
