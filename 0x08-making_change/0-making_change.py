018528fba21e79a1195b9bc14e9f125d6cd22e05
Samuel <themainsamuel@gmail.com>
2024-11-29 02:04:39 +0100

    requirement check

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-# function to find the minimum number of coins required to make up a given total amount, given a list of coin denominations
+# function to find the minimum number of coins required to make
+# up a given total amount, given a list of coin denominations
 
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    # create a list to store the minimum number of coins required for each amount from 0 to
-    # the given total amount
+    # create a list to store the minimum number of coins required 
+    # for each amount from 0 to the given total amount
     dp = [float('inf')] * (total + 1)
     dp[0] = 0  # 0 coins are required to make up an amount of 0
     for coin in coins:
